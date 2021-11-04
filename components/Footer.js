e07f33404e32a1f5ec9c048afbf1fa0eff73069f
benjaminmountain <bemo1787@colorado.edu>
2021-11-04 14:03:10 -0400

    footer update

--- components/Footer.js
+++ components/Footer.js
@@ -6,27 +6,27 @@ function Footer() {
     >
       <div className="space-y-4 text-xs text-gray-800">
         <h5 className="font-bold">ABOUT</h5>
-        <p>How Airbnb works</p>
-        <p>Newsroom</p>
-        <p>Investors</p>
-        <p>Airbnb Plus</p>
-        <p>Airbnb Luxe</p>
+        <p>Not affiliated with Airbnb</p>
+        <p>Demo Build</p>
+        <p>For educational</p>
+        <p>And portfolio</p>
+        <p>Purposes</p>
       </div>
       <div className="space-y-4 text-xs text-gray-800">
         <h5 className="font-bold">COMMUNITY</h5>
-        <p>How Airbnb works</p>
-        <p>Newsroom</p>
-        <p>Investors</p>
-        <p>Airbnb Plus</p>
-        <p>Airbnb Luxe</p>
+        <p>Thanks to</p>
+        <p>Sonny Sangha</p>
+        <p>For assistance</p>
+        <p>With this build</p>
+        <p>And much more</p>
       </div>
       <div className="space-y-4 text-xs text-gray-800">
         <h5 className="font-bold">HOST</h5>
-        <p>How Airbnb works</p>
-        <p>Newsroom</p>
-        <p>Investors</p>
-        <p>Airbnb Plus</p>
-        <p>Airbnb Luxe</p>
+        <p>Do not try</p>
+        <p>To become a host</p>
+        <p>Through this site</p>
+        <p>It will not work</p>
+        <p>We promise</p>
       </div>
       <div className="space-y-4 text-xs text-gray-800">
         <h5 className="font-bold">SUPPORT</h5>
